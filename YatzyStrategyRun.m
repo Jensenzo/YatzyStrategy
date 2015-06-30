4ed92829641838333ff79e3a188d227a246e368e
Jens Enzo Nyby Christensen <jens@jensenzo.com>
2015-06-30 02:05:47 +0200

    Idea notes

--- YatzyStrategyRun.m
+++ YatzyStrategyRun.m
@@ -5,6 +5,11 @@ load dice dice
 
 %% Strategy
 
+%Ideas:
+% - Generate helper classes (straights)
+% - Strategies could be different priorities of "keys" on the sheet
+% - Track which "keys" are thrown away in each game (bonus as well)
+
 
 
 %% Run
